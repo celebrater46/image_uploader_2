00d0d867636955f51e844d3267fba5f3f919b040
celebrater <tkgm45nbchk@gmail.com>
2022-03-27 06:23:40 +0900

    fixed bug "img" directory does not exist

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ session_start();
 ini_set('display_errors', 1); // xampp みたいなエラーメッセージ出してくれる
 define('MAX_FILE_SIZE', 1 * 1024 * 1024); // 1MB
 define('THUMBNAIL_WIDTH', 400);
-define('IMAGES_DIR', __DIR__ . '/img'); // 画像ファイルのディレクトリ（__DIR__ は現在のディレクトリ取得）
-define('THUMBNAIL_DIR', __DIR__ . '/s'); // サムネイルディレクトリ
-// define('IMAGES_DIR', 'http://localhost/PG/DotInstall/PHP/php7/img');
-// define('THUMBNAIL_DIR', 'http://localhost/PG/DotInstall/PHP/php7/s');
-// define('IMAGES_DIR', 'C:\xampp\htdocs\PG\DotInstall\PHP\php7\img'); // ローカル開発環境のディレクトリはこちらが正解
-// define('THUMBNAIL_DIR', 'C:\xampp\htdocs\PG\DotInstall\PHP\php7\s');
+//define('IMAGES_DIR', __DIR__ . '/img'); // 画像ファイルのディレクトリ（__DIR__ は現在のディレクトリ取得）
+//define('THUMBNAIL_DIR', __DIR__ . '/s'); // サムネイルディレクトリ
+// define('IMAGES_DIR', 'http://localhost/myapps/image_uploader/img');
+// define('THUMBNAIL_DIR', 'http://localhost/myapps/image_uploader/s');
+define('IMAGES_DIR', 'J:\Dropbox\PC5_cloud\pg\xampp\htdocs\myapps\image_uploader\img'); // ローカル開発環境のディレクトリはこちらが正解
+define('THUMBNAIL_DIR', 'J:\Dropbox\PC5_cloud\pg\xampp\htdocs\myapps\image_uploader\s');
 
 // 画像処理には GD というプラグインが必要。入っているかどうかのチェック
 if (!function_exists('imagecreatetruecolor')) {
